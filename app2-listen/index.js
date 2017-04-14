f824bd9792b7779e1fe3c5f33a951e4c6d65452c
Jake Headley <jheadley@roomkey.com>
2017-04-14 10:26:36 -0600

    Committing app 2 drill

---
+++ app2-listen/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+,     bodyParser = require('body-parser')
+,      cors = require = ('cors');
+
+
+const app = express();
+
+
+const PORT = 3000;
+app.listen(PORT, console.log('Listening on port:', PORT, 'yAy\!'));
